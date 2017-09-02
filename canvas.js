09c635abfdf7f6d0fe95f830b2c58c329255c188
Luis Alberto Luisjuan Guerrero <alberto.luisjuan@cusur.udg.mx>
2017-09-02 12:47:34 -0500

    Cuando el evento touchcancel ocurre se activa un evento mouseup.

--- canvas.js
+++ canvas.js
@@ -6,16 +6,19 @@
 		http://bencentra.com/canvas/signature/signature.html - https://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
 */
 
-(function() { // Comenzamos una funcion auto-ejecutable
+(function()
+{ // Comenzamos una funcion auto-ejecutable
 
 	// Obtenenemos un intervalo regular(Tiempo) en la pamtalla
-	window.requestAnimFrame = (function (callback) {
+	window.requestAnimFrame = (function (callback)
+	{
 		return window.requestAnimationFrame ||
 					window.webkitRequestAnimationFrame ||
 					window.mozRequestAnimationFrame ||
 					window.oRequestAnimationFrame ||
 					window.msRequestAnimaitonFrame ||
-					function (callback) {
+					function (callback)
+					{
 					 	window.setTimeout(callback, 1000/60);
             // Retrasa la ejecucion de la funcion para mejorar la experiencia
 					};
@@ -31,13 +34,15 @@
 	var drawImage = document.getElementById("draw-image");
 	var clearBtn = document.getElementById("draw-clearBtn");
 	var submitBtn = document.getElementById("draw-submitBtn");
-	clearBtn.addEventListener("click", function (e) {
+	clearBtn.addEventListener("click", function (e)
+	{
 		// Definimos que pasa cuando el boton draw-clearBtn es pulsado
 		clearCanvas();
 		drawImage.setAttribute("src", "");
 	}, false);
 		// Definimos que pasa cuando el boton draw-submitBtn es pulsado
-	submitBtn.addEventListener("click", function (e) {
+	submitBtn.addEventListener("click", function (e)
+	{
 	var dataUrl = canvas.toDataURL();
 	drawText.innerHTML = dataUrl;
 	drawImage.setAttribute("src", dataUrl);
@@ -69,68 +74,88 @@
 	}, false);
 
 	// Activamos touchEvent para nuestra pagina
-	canvas.addEventListener("touchstart", function (e) {
+	canvas.addEventListener("touchstart", function (e)
+	{
 		mousePos = getTouchPos(canvas, e);
     console.log(mousePos);
     e.preventDefault(); // Prevent scrolling when touching the canvas
 		var touch = e.touches[0];
-		var mouseEvent = new MouseEvent("mousedown", {
+		var mouseEvent = new MouseEvent("mousedown",
+		{
 			clientX: touch.clientX,
 			clientY: touch.clientY
 		});
 		canvas.dispatchEvent(mouseEvent);
 	}, false);
-	canvas.addEventListener("touchend", function (e) {
+	canvas.addEventListener("touchend", function (e)
+	{
     e.preventDefault(); // Prevent scrolling when touching the canvas
 		var mouseEvent = new MouseEvent("mouseup", {});
 		canvas.dispatchEvent(mouseEvent);
 	}, false);
-  canvas.addEventListener("touchleave", function (e) {
+  canvas.addEventListener("touchleave", function (e)
+	{
     // Realiza el mismo proceso que touchend en caso de que el dedo se deslice fuera del canvas
     e.preventDefault(); // Prevent scrolling when touching the canvas
     var mouseEvent = new MouseEvent("mouseup", {});
     canvas.dispatchEvent(mouseEvent);
   }, false);
-	canvas.addEventListener("touchmove", function (e) {
+	canvas.addEventListener("touchcancel", function (e)
+	{
+    // Realiza el mismo proceso que touchend en caso de que el dedo se deslice fuera del canvas
+    e.preventDefault(); // Prevent scrolling when touching the canvas
+    var mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+  }, false);
+	canvas.addEventListener("touchmove", function (e)
+	{
     e.preventDefault(); // Prevent scrolling when touching the canvas
 		var touch = e.touches[0];
-		var mouseEvent = new MouseEvent("mousemove", {
+		var mouseEvent = new MouseEvent("mousemove",
+		{
 			clientX: touch.clientX,
 			clientY: touch.clientY
 		});
 		canvas.dispatchEvent(mouseEvent);
 	}, false);
 
+
 	// Get the position of the mouse relative to the canvas
-	function getMousePos(canvasDom, mouseEvent) {
+	function getMousePos(canvasDom, mouseEvent)
+	{
 		var rect = canvasDom.getBoundingClientRect();
     /*
       Devuelve el tamaño de un elemento y su posición relativa respecto
       a la ventana de visualización (viewport).
     */
-		return {
+		return
+		{
 			x: mouseEvent.clientX - rect.left,
 			y: mouseEvent.clientY - rect.top
 		};
 	}
 
 	// Get the position of a touch relative to the canvas
-	function getTouchPos(canvasDom, touchEvent) {
+	function getTouchPos(canvasDom, touchEvent)
+	{
 		var rect = canvasDom.getBoundingClientRect();
     console.log(touchEvent);
     /*
       Devuelve el tamaño de un elemento y su posición relativa respecto
       a la ventana de visualización (viewport).
     */
-		return {
+		return
+		{
 			x: touchEvent.touches[0].clientX - rect.left, // Popiedad de todo evento Touch
 			y: touchEvent.touches[0].clientY - rect.top
 		};
 	}
 
 	// Draw to the canvas
-	function renderCanvas() {
-		if (drawing) {
+	function renderCanvas()
+	{
+		if (drawing)
+		{
       var tint = document.getElementById("color");
       var punta = document.getElementById("puntero");
       ctx.strokeStyle = tint.value;
@@ -145,12 +170,14 @@
 		}
 	}
 
-	function clearCanvas() {
+	function clearCanvas()
+	{
 		canvas.width = canvas.width;
 	}
 
 	// Allow for animation
-	(function drawLoop () {
+	(function drawLoop ()
+	{
 		requestAnimFrame(drawLoop);
 		renderCanvas();
 	})();
